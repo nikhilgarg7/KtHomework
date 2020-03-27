3baa448c371426db83ed5c963311643303476c9d
harsh183 <hrshdeep51@gmail.com>
2020-03-27 16:54:18 -0500

    Initialize repository

---
+++ src/Preliminaries/Printing.kt
@@ -0,0 +1,13 @@
+package Preliminaries
+
+// Goal to print out
+// Hello, World
+// Kotlin sparks joy
+// Programming should spark joy :)
+fun main() {
+    println("Hello, World")
+    println("EDIT")
+    print("Programming should spark")
+    print("EDIT")
+    println(":)")
+}
