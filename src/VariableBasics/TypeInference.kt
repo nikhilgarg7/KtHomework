005b087365be952770bf65c1e6b36f7795e083fc
harsh183 <hrshdeep51@gmail.com>
2020-03-27 19:03:48 -0500

    Type inference problems

---
+++ src/VariableBasics/TypeInference.kt
@@ -0,0 +1,35 @@
+package VariableBasics
+
+import TestUtil.Case
+import TestUtil.runCases
+
+// There is no really good way to test if you are using type inference or not
+// so basically the goal here is to remove all type declares and still have
+// a full score
+fun main() {
+    val cases = listOf(
+        Case("Declaring random variables") {
+            val a: Int = 5
+            val b: Int = 10
+            val name: String = "CS 199 IKP"
+            val gpa: Float = 4.0f
+            val score: Double = 99.8
+
+            a == 5 && b == 10 && name == "CS 199 IKP" && gpa == 4.0f && score == 99.8
+        },
+
+        Case("Declaring lists") {
+            val array1: Array<Int> = arrayOf(0, 1, 4, 9, 16)
+            val list1: List<Int> = listOf(0, 1, 4, 9, 16)
+            val map1: Map<Int, Int> = mapOf(0 to 0, 1 to 1, 2 to 4, 3 to 9, 4 to 16)
+
+            val array2: Array<Int> = Array(5) { it*it }
+            val list2: List<Int> = List(5) {it*it}
+            val map2: Map<Int, Int> = (Array(5) {it}).associate {it to it*it}
+
+            array1.contentEquals(array2) && list1 == list2 && map1 == map2
+        }
+    )
+
+    runCases(cases)
+}
